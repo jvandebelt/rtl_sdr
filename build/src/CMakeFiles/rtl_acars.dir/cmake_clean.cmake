131e82bdb492ea7490c270fecf64f28a1778c5bf
Michael Mende <michael@x41.(none)>
2013-07-16 22:01:15 +0200

    initial commit of rtl_sdr with additional rtl_acars

---
+++ build/src/CMakeFiles/rtl_acars.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rtl_acars.dir/rtl_acars.c.o"
+  "rtl_acars.pdb"
+  "rtl_acars"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/rtl_acars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/src/CMakeFiles/rtl_sdr.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/michael/workspace/c/rtl-sdr/src/rtl_sdr.c" "/home/michael/workspace/c/rtl-sdr/build/src/CMakeFiles/rtl_sdr.dir/rtl_sdr.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/michael/workspace/c/rtl-sdr/build/src/CMakeFiles/rtlsdr_shared.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/libusb-1.0"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
